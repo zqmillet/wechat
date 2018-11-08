a97ef90b5c2c75cdc74e0340bc8bcde2f7a56263
zqmillet <zqmillet@qq.com>
2018-11-08 23:37:20 +0800

    modify the file main.py

--- main.py
+++ main.py
@@ -1,29 +1,70 @@
 import urwid
 
-def question():
-    return urwid.Pile([urwid.Edit(('I say', u"What is your name?\n"))])
+def menu_button(caption, callback):
+    button = urwid.Button(caption)
+    urwid.connect_signal(button, 'click', callback)
+    return urwid.AttrMap(button, None, focus_map='reversed')
 
-def answer(name):
-    return urwid.Text(('I say', u"Nice to meet you, " + name + "\n"))
+def sub_menu(caption, choices):
+    contents = menu(caption, choices)
+    def open_menu(button):
+        return top.open_box(contents)
+    return menu_button([caption, u'...'], open_menu)
 
-class ConversationListBox(urwid.ListBox):
-    def __init__(self):
-        body = urwid.SimpleFocusListWalker([question()])
-        super(ConversationListBox, self).__init__(body)
+def menu(title, choices):
+    body = [urwid.Text(title), urwid.Divider()]
+    body.extend(choices)
+    return urwid.ListBox(urwid.SimpleFocusListWalker(body))
+
+def item_chosen(button):
+    response = urwid.Text([u'You chose ', button.label, u'\n'])
+    done = menu_button(u'Ok', exit_program)
+    top.open_box(urwid.Filler(urwid.Pile([response, done])))
+
+def exit_program(button):
+    raise urwid.ExitMainLoop()
+
+menu_top = menu(u'Main Menu', [
+    sub_menu(u'Applications', [
+        sub_menu(u'Accessories', [
+            menu_button(u'Text Editor', item_chosen),
+            menu_button(u'Terminal', item_chosen),
+        ]),
+    ]),
+    sub_menu(u'System', [
+        sub_menu(u'Preferences', [
+            menu_button(u'Appearance', item_chosen),
+        ]),
+        menu_button(u'Lock Screen', item_chosen),
+    ]),
+])
+
+class CascadingBoxes(urwid.WidgetPlaceholder):
+    max_box_levels = 4
+
+    def __init__(self, box):
+        super(CascadingBoxes, self).__init__(urwid.SolidFill(u'/'))
+        self.box_level = 0
+        self.open_box(box)
+
+    def open_box(self, box):
+        self.original_widget = urwid.Overlay(urwid.LineBox(box),
+            self.original_widget,
+            align='center', width=('relative', 80),
+            valign='middle', height=('relative', 80),
+            min_width=24, min_height=8,
+            left=self.box_level * 3,
+            right=(self.max_box_levels - self.box_level - 1) * 3,
+            top=self.box_level * 2,
+            bottom=(self.max_box_levels - self.box_level - 1) * 2)
+        self.box_level += 1
 
     def keypress(self, size, key):
-        key = super(ConversationListBox, self).keypress(size, key)
-        if key != 'enter':
-            return key
-        name = self.focus[0].edit_text
-        if not name:
-            raise urwid.ExitMainLoop()
-        # replace or add response
-        self.focus.contents[1:] = [(answer(name), self.focus.options())]
-        pos = self.focus_position
-        # add a new question
-        self.body.insert(pos + 1, question())
-        self.focus_position = pos + 1
+        if key == 'esc' and self.box_level > 1:
+            self.original_widget = self.original_widget[0]
+            self.box_level -= 1
+        else:
+            return super(CascadingBoxes, self).keypress(size, key)
 
-palette = [('I say', 'default,bold', 'default'),]
-urwid.MainLoop(ConversationListBox(), palette).run()
+top = CascadingBoxes(menu_top)
+urwid.MainLoop(top, palette=[('reversed', 'standout', '')]).run()
