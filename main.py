8f110640934cd1042043adca95759f3483b24f99
zqmillet <zqmillet@qq.com>
2018-11-14 00:05:00 +0800

    modify the file main.py

--- main.py
+++ main.py
@@ -3,8 +3,19 @@ import urwid
 def show_or_exit(key):
     if key in ('q', 'Q'):
         raise urwid.ExitMainLoop()
-    text.set_text(repr(key))
+    text.set_text(text.get_text()[0] + key)
+    if key == 'r':
+        text.set_align_mode('right')
 
-text = urwid.Text('Hello World')
-fill = urwid.Filler(text, 'top')
-urwid.MainLoop(fill, unhandled_input = show_or_exit).run()
+pelette = {
+    ('banner', 'black', 'light gray'),
+    ('streak', 'black', 'dark red'),
+    ('bg', 'black', 'dark blue')
+}
+
+text = urwid.Text(('banner', 'Hello World'), align = 'center')
+map1 = urwid.AttrMap(text, 'streak')
+fill = urwid.Filler(map1)
+map2 = urwid.AttrMap(fill, 'bg')
+loop = urwid.MainLoop(map2, pelette, unhandled_input = show_or_exit)
+loop.run()
