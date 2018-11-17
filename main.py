0771bc6342164ffe351ad91384c86be99a1a30d5
zqmillet <zqmillet@qq.com>
2018-11-17 21:26:21 +0800

    modify the file main.py

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import urwid
 palette = [('i say', 'default, bold', 'default', 'bold'),]
 ask1 = urwid.Edit(('i say', 'what is your name?\n'))
 ask2 = urwid.Edit(('i say', 'what is your name?\n'))
-top = urwid.Columns([ask1, ask2], 1)
+top = urwid.Columns([ask1, ask2])
 
 urwid.MainLoop(top, palette).run()
