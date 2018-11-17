694e3def6ddeb6a81ec80bb6d17b35f98113850e
zqmillet <zqmillet@qq.com>
2018-11-17 21:25:46 +0800

    modify the file main.py

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
 import urwid
 
 palette = [('i say', 'default, bold', 'default', 'bold'),]
-ask = urwid.Edit(('i say', 'what is your name?\n'))
-reply = urwid.Text('')
-button = urwid.Button('exit')
-div = urwid.Divider()
-pile = urwid.Pile([ask, div, reply, div, button])
-top = urwid.Columns([pile, div], 1)
-
-def on_ask_change(edit, new_edit_text):
-    reply.set_text(('i say', 'nice to meet you, ' + new_edit_text))
-
-def on_exit_click(button):
-    raise urwid.ExitMainLoop()
-
-urwid.connect_signal(ask, 'change', on_ask_change)
-urwid.connect_signal(button, 'click', on_exit_click)
+ask1 = urwid.Edit(('i say', 'what is your name?\n'))
+ask2 = urwid.Edit(('i say', 'what is your name?\n'))
+top = urwid.Columns([ask1, ask2], 1)
 
 urwid.MainLoop(top, palette).run()
