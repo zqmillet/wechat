691be135aa8ade03a129d5048f260df04aa4487d
zqmillet <zqmillet@qq.com>
2018-11-18 23:51:49 +0800

    modify the file main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import urwid
 
-ask = urwid.Edit('what is your name?\n')
-top = urwid.Filler(ask)
+widget1 = urwid.Edit('what is your name?\n')
+widget1 = urwid.LineBox(widget1)
+widget1 = urwid.Padding(widget1, align = 'left', width = ('relative', 100))
+widget1 = urwid.Filler(widget1)
 
-urwid.MainLoop(urwid.Columns([urwid.Pile([top, top]), top, top], 10)).run()
+widget2 = urwid.Edit('what is your name?\n')
+widget2 = urwid.LineBox(widget2)
+widget2 = urwid.Padding(widget2, align = 'left', width = ('relative', 100))
+widget2 = urwid.Filler(widget2)
+
+
+urwid.MainLoop(urwid.Columns([widget1, widget2])).run()
