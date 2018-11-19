6f898fbbdcb61da94e2ad10608a38e7031fe8684
zqmillet <zqmillet@qq.com>
2018-11-20 00:16:36 +0800

    modify the file main.py

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import urwid
 
 widget1 = urwid.Edit('what is your name?\n')
 widget1 = urwid.LineBox(widget1)
-widget1 = urwid.Padding(widget1, align = 'left', width = ('relative', 100))
-widget1 = urwid.Filler(widget1, height = ('relative', 10), valign = 'top')
+widget1 = urwid.Padding(widget1)
+widget1 = urwid.Filler(widget1)
 
 widget2 = urwid.Edit('what is your name?\n')
 widget2 = urwid.LineBox(widget2)
-widget2 = urwid.Padding(widget2, align = 'left', width = 'pack')
-widget2 = urwid.Filler(widget2, height = ('relative', 10), valign = 'top')
+widget2 = urwid.Padding(widget2)
+widget2 = urwid.Filler(widget2)
 
+vline = urwid.AttrWrap( urwid.SolidFill(u'\u2502'), 'line')
 
-urwid.MainLoop(urwid.Columns([urwid.Pile([widget1, widget2]), widget1, widget2])).run()
+urwid.MainLoop(urwid.Columns([('weight', 1, widget1), ('fixed', 1, vline), ('weight', 2, widget2)])).run()
