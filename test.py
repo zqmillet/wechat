0a8f84aa6b2dd9323238fdcad4c77828399bfb73
zqmillet <zqmillet@qq.com>
2018-11-25 00:32:32 +0800

    add the file text.py

---
+++ test.py
@@ -0,0 +1,19 @@
+import urwid
+
+text1 = urwid.Text('2345')
+text2 = urwid.Text('23333', align = 'right')
+
+filler1 = urwid.Filler(text1)
+filler2 = urwid.Filler(text2)
+
+class ChatListBox(urwid.ListBox):
+    def __init__(self, widget_list):
+        super(ChatListBox, self).__init__(urwid.SimpleListWalker(widget_list))
+
+listbox = ChatListBox([
+    text1,
+    text2,
+    urwid.Divider('─', top = 0),
+    text1,
+    text2])
+urwid.MainLoop(listbox).run()
