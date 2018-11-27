930efb91ede5a252a921b5b90e1b0add965efc4c
zqmillet <zqmillet@qq.com>
2018-11-27 21:43:19 +0800

    modify the file test.py

--- test.py
+++ test.py
@@ -6,6 +6,14 @@ text2 = urwid.Text('23333', align = 'right')
 filler1 = urwid.Filler(text1)
 filler2 = urwid.Filler(text2)
 
+class ChatListItem(object):
+    name = None
+    text = None
+
+    def __init__(self, name, text):
+        self.name = name
+        self.text = name
+
 class ChatListBox(urwid.ListBox):
     def __init__(self, widget_list):
         super(ChatListBox, self).__init__(urwid.SimpleListWalker(widget_list))
