e5a1c258a57a9a0487e33806e1a4f82233e94aef
zqmillet <zqmillet@qq.com>
2018-11-28 22:48:53 +0800

    modify the file test.py

--- test.py
+++ test.py
@@ -6,20 +6,22 @@ text2 = urwid.Text('23333', align = 'right')
 filler1 = urwid.Filler(text1)
 filler2 = urwid.Filler(text2)
 
-class ChatListItem(object):
+class ChatListItem(urwid.Filler):
     name = None
     text = None
 
     def __init__(self, name, text):
         self.name = name
-        self.text = name
+        self.text = text
+        import pdb; pdb.set_trace()
+        super(ChatListItem, self).__init__(text1)
 
 class ChatListBox(urwid.ListBox):
     def __init__(self, widget_list):
         super(ChatListBox, self).__init__(urwid.SimpleListWalker(widget_list))
 
 listbox = ChatListBox([
-    text1,
+    ChatListItem('123', '333'),
     text2,
     urwid.Divider('─', top = 0),
     text1,
