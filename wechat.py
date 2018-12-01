e7de5c5572421859efabd329397c32da120f0dd7
zqmillet <zqmillet@qq.com>
2018-12-02 00:56:53 +0800

    modify the file dictionary.py

--- wechat.py
+++ wechat.py
@@ -1,3 +1,13 @@
 import urwid
 
 from dictionary import Dictionary
+
+class ChatListBox(urwid.Filler):
+    pass
+
+text = urwid.Text('12345')
+filler = urwid.Filler(text)
+linebox = urwid.LineBox(filler)
+
+urwid.MainLoop(linebox).run()
+
