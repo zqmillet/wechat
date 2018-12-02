760339afb2ac9902a5fb0016601797f3de19f145
zqmillet <zqmillet@qq.com>
2018-12-03 00:41:56 +0800

    modify the file dictionary.py

--- wechat.py
+++ wechat.py
@@ -6,7 +6,7 @@ class ChatListBox(urwid.Filler):
     pass
 
 text = urwid.Text('12345')
-filler = urwid.Filler(text)
+filler = urwid.Filler(text, height = 1)
 linebox = urwid.LineBox(filler, title = '1234', title_align = 'left')
 
 urwid.MainLoop(linebox).run()
