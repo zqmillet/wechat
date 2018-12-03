34f48d1dacf7c3f8cafe58b88181c93f612b1a8d
zqmillet <zqmillet@qq.com>
2018-12-04 01:06:52 +0800

    modify the file wechat.py

--- wechat.py
+++ wechat.py
@@ -9,5 +9,10 @@ text = urwid.Text('12345', align = 'right')
 linebox = urwid.LineBox(text, title = '1234', title_align = 'left')
 listbox = urwid.ListBox(urwid.SimpleListWalker([linebox, linebox, linebox]))
 
-urwid.MainLoop(listbox).run()
+detaillinebox = urwid.LineBox(urwid.Text(''), title = 'jajaj', title_align = 'left')
+detaillistbox = urwid.ListBox(urwid.SimpleListWalker([detaillinebox, detaillinebox]))
+
+columns = urwid.Columns([(20, listbox), detaillistbox])
+
+urwid.MainLoop(columns).run()
 
