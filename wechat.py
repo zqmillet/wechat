fca06b26e653e8f9981b07f0fb9f1613b0b0ee7a
zqmillet <zqmillet@qq.com>
2018-12-04 00:52:31 +0800

    modify the file wechat.py

--- wechat.py
+++ wechat.py
@@ -5,9 +5,9 @@ from dictionary import Dictionary
 class ChatListBox(urwid.Filler):
     pass
 
-text = urwid.Text('12345')
-filler = urwid.Filler(text, height = 1)
-linebox = urwid.LineBox(filler, title = '1234', title_align = 'left')
+text = urwid.Text('12345', align = 'right')
+linebox = urwid.LineBox(text, title = '1234', title_align = 'left')
+listbox = urwid.ListBox(urwid.SimpleListWalker([linebox, linebox, linebox]))
 
-urwid.MainLoop(linebox).run()
+urwid.MainLoop(listbox).run()
 
